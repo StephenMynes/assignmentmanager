1a50a8369aa8df162ddbdf2fdbf08c9cf5848e95
StephenMynes <45215977+StephenMynes@users.noreply.github.com>
2018-11-20 22:59:55 +0000

    Delete File Maker.py

--- File Maker.py
+++
@@ -1,8 +0,0 @@
-#This makes the initial CSV file for testing purposes
-import csv
-
-array = [["Assignment for: Mike", "Unit: Unit 20", "Due: 30/11/2018", "----------------"]]
-
-with open("assignments.csv", "w") as db:
-    writer = csv.writer(db, lineterminator = "\n")
-    writer.writerows(array)
